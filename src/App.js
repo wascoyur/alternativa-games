3e6c25ebd5b8206e34f753ac7fc3642e2d961859
wascoyur <mytem20007@gmail.com>
2022-04-05 03:47:06 +0500

    Add aticles and profile pages

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Header from './components/Header';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import { Container } from 'react-bootstrap';
+import Articles from './components/Articles';
+import Profile from './components/Profile';
 
 function App() {
   return (
@@ -13,6 +15,8 @@ function App() {
         <Container>
           <Routes>
             <Route path='/' element={<Home />} exact />
+            <Route path='/articles' element={<Articles />} exact />
+            <Route path='/profile' element={<Profile />} exact />
           </Routes>
         </Container>
       </main>
