b394426e96291802738a86edb2897687ce51cac9
wascoyur <mytem20007@gmail.com>
2022-04-08 21:10:52 +0500

    deploy

--- src/assets/articles.js
+++ src/assets/articles.js
@@ -2,15 +2,18 @@ export const articles = [
   {
     id: 1,
     title: 'Есть ли жизнь на Марсе?',
-    content: 'Qui adipisicing officia qui dolore et nulla do id.',
+    content: `Veniam ullamco amet sit irure consectetur pariatur eu labore eu sint non incididunt amet. Mollit ea labore magna aliqua fugiat fugiat in magna adipisicing do enim dolor. Esse est excepteur aliqua ipsum occaecat et aute. Id anim sit nisi sit velit id veniam ipsum proident cupidatat cupidatat cupidatat. Reprehenderit nostrud non anim exercitation id voluptate esse id veniam.
+      
+    Quis tempor dolore ad ex mollit sunt amet amet. Commodo excepteur et dolore incididunt magna cupidatat anim dolore eu laboris non. Aute proident occaecat officia commodo non eiusmod quis quis proident deserunt enim do. Duis ut pariatur duis aliquip nisi mollit duis Lorem ullamco quis eiusmod est ullamco dolor. Cupidatat non nulla quis consectetur. Culpa tempor do eiusmod proident nostrud voluptate est labore proident sint..`,
     tag: 'space',
     author: 'Ted',
   },
   {
     id: 2,
     title: 'Одиноки ли мы во Вселенной?',
-    content:
-      'Sit in elit ullamco pariatur ea Lorem duis aliqua veniam minim adipisicing ex incididunt.',
+    content: `Sit in elit ullamco pariatur ea Lorem duis aliqua veniam minim adipisicing ex incididunt.
+      Officia duis et id esse aliquip. Labore eu do duis deserunt sunt eiusmod ea velit ex ex sint ut cupidatat aliqua. Magna velit amet duis veniam commodo in est irure proident laborum. Duis cupidatat consequat laboris excepteur laboris tempor id laboris dolor amet. Labore tempor dolore duis ex ipsum velit occaecat amet qui aute eu fugiat sit enim. Quis cillum irure sit eu cupidatat eu anim laboris.
+      `,
     tag: 'space',
     author: 'Koto',
   },
